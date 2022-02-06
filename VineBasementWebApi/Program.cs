61358648c04b91f734b79f7952b09a54cd433a15
SternikBB <45210186+SternikBB@users.noreply.github.com>
2022-02-06 22:33:56 +0100

    test

--- VineBasementWebApi/Program.cs
+++ VineBasementWebApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer()
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
